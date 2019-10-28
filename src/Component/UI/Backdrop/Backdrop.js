334b5a8213ca0c928550df452697546f482de45f
Sergey <popenko.sergey08@gmail.com>
2019-10-28 12:16:54 +0300

    Dimming component

---
+++ src/Component/UI/Backdrop/Backdrop.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import classes from './Backdrop.module.scss';
+
+const Backdrop = props => {
+    console.log(props)
+    return (
+        <div
+            className={classes.Backdrop}
+            onClick={props.onClick}
+        />
+
+    )
+};
+
+export default Backdrop;
