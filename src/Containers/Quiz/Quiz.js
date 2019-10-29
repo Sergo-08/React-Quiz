6567df60b6ae37b566b578c3dff216f059d963ee
Sergey <popenko.sergey08@gmail.com>
2019-10-29 14:02:38 +0300

    List page

--- src/Containers/Quiz/Quiz.js
+++ src/Containers/Quiz/Quiz.js
@@ -90,7 +90,9 @@ class Quiz extends Component {
         return this.state.activeQuestion + 1 === this.state.quiz.length
     }
 
-
+    componentDidMount() {
+        console.log('Quiz ID = ', this.props.match.params.id)
+    }
     render() {
         return (
             <div className={classes.Quiz}>
