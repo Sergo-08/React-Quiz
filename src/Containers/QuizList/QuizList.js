e3349b5959796f696de5e82fb8187965a1a65f14
Sergey <popenko.sergey08@gmail.com>
2019-10-31 17:58:41 +0300

    Download test list server

--- src/Containers/QuizList/QuizList.js
+++ src/Containers/QuizList/QuizList.js
@@ -4,24 +4,41 @@ import {NavLink} from "react-router-dom";
 import axios from 'axios';
 
 class QuizList extends Component {
+    state = {
+        quizes: [],
+
+    };
+
     renderQuizes() {
-        return [1, 2, 3].map((quiz, index) => {
+        return this.state.quizes.map((quiz) => {
             return (
                 <li
-                    key={index}
+                    key={quiz.id}
                 >
-                    <NavLink to={'/quiz/' + quiz}>
-                        Тест {quiz}
+                    <NavLink to={'/quiz/' + quiz.id}>
+                        {quiz.name}
                     </NavLink>
                 </li>
             )
         })
     }
 
-    componentDidMount() {
-        axios.get('https://react-quiz-6a4c5.firebaseio.com/quiz.json').then(response => {
-            console.log(response)
-        })
+    async componentDidMount() {
+        try {
+            const response = await axios.get('https://react-quiz-6a4c5.firebaseio.com/quizes.json');
+            const quizes = [];
+            Object.keys(response.data).forEach((key, index) => {
+                quizes.push({
+                    id: key,
+                    name: `Тест №${index + 1}`
+                });
+                this.setState({
+                    quizes,
+                })
+            })
+        } catch (e) {
+                console.log(e)
+        }
     }
 
     render() {
