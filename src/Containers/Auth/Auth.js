d044e96604e1a238432837bd8adf20d98d72ae02
Sergey <popenko.sergey08@gmail.com>
2019-11-01 13:56:21 +0300

    Login

--- src/Containers/Auth/Auth.js
+++ src/Containers/Auth/Auth.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import classes from './Auth.module.scss'
 import Button from "../../Component/UI/Button/Button";
 import Input from "../../Component/UI/Input/Input";
+import axios from '../../axios/axios-quiz';
 
 const validateEmail = (email) => {
     const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -23,7 +24,7 @@ class Auth extends Component {
                     required: true,
                     email: true
                 }
-                },
+            },
             password: {
                 value: '',
                 type: 'password',
@@ -39,12 +40,32 @@ class Auth extends Component {
         }
     };
 
-    loginHandler = () => {
-
+    loginHandler = async   () => {
+        const authData = {
+            email: this.state.formControls.email.value,
+            password: this.state.formControls.password.value,
+            returnSecureToken: true,
+        };
+        try {
+            const response = await axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDOgykdItq1gFveim2iAPL4TC-0tv0lEew', authData)
+            console.log(response.data)
+        } catch (e) {
+            console.log(e)
+        }
     };
 
-    registerHandler = () => {
-
+    registerHandler = async () => {
+        const authData = {
+            email: this.state.formControls.email.value,
+            password: this.state.formControls.password.value,
+            returnSecureToken: true,
+        };
+        try {
+            const response = await axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDOgykdItq1gFveim2iAPL4TC-0tv0lEew', authData)
+console.log(response.data)
+        } catch (e) {
+            console.log(e)
+        }
     };
 
     submitHandler = (event) => {
